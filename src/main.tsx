e0be87c7de720807f97922c8d6f38a55b8c1c670
derrick <drocha616@gmail.com>
2026-06-06 21:07:37 -0600

    removed use v8.

--- src/main.tsx
+++ src/main.tsx
@@ -37,12 +37,7 @@ const router = createBrowserRouter([
             }
         ],
     },
-], {
-    future: {
-        v8_middleware: true,  // Required for middleware to work
-        // Or use unstable_middleware: true depending on your version
-    }
-});
+]);
 
 async function authMiddleware() {
     const token = localStorage.getItem('accessToken');
